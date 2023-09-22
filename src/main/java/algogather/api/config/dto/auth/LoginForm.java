34ea73f630d8bbaaaa39a102923015a04f62187a
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-22 13:38:23 +0900

    feat: LoginForm, SignUpForm 구현

--- src/main/java/algogather/api/config/dto/auth/LoginForm.java
+++ src/main/java/algogather/api/config/dto/auth/LoginForm.java
@@ -1,4 +1,17 @@
 package algogather.api.config.dto.auth;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
 public class LoginForm {
+    private String userId;
+    private String password;
+
+    @Builder
+    public LoginForm(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
 }
--- src/main/java/algogather/api/config/dto/auth/SignUpForm.java
+++ src/main/java/algogather/api/config/dto/auth/SignUpForm.java
@@ -1,4 +1,22 @@
 package algogather.api.config.dto.auth;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class SignUpForm {
+    private String userId;
+    private String password;
+    private String name;
+    private String profile_image;
+    private String judge_account;
+
+    @Builder
+    public SignUpForm(String userId, String password, String name, String profile_image, String judge_account) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.profile_image = profile_image;
+        this.judge_account = judge_account;
+    }
 }
