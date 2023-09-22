71ef44af0a4de227d051ce18b412bc9d19067919
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-22 12:27:42 +0900

    feat: user 패키지 도메인 구현 완료

--- src/main/java/algogather/api/domain/user/User.java
+++ src/main/java/algogather/api/domain/user/User.java
@@ -18,8 +18,22 @@ public class User extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "user_id", nullable = false, unique = true)
+    private String userId;
+
+    @Column(name = "user_password", nullable = false)
+    private String password;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Column(name = "user_profile_image")
+    private String profile_image;
+
+    @Column(name = "user_judge_account", nullable = false)
+    private String judge_account;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "user_role")
     private UserRole role;
-
 }
