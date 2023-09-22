8172e7cec6f2728ae4716e6d0123f025be6202da
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-22 12:41:17 +0900

    feat: assignment 패키지 도메인 구현 완료

--- src/main/java/algogather/api/domain/assignment/AssignmentProblem.java
+++ src/main/java/algogather/api/domain/assignment/AssignmentProblem.java
@@ -1,12 +1,15 @@
 package algogather.api.domain.assignment;
 
 import algogather.api.domain.BaseTimeEntity;
+import algogather.api.domain.problem.Problem;
+import algogather.api.domain.studyroom.StudyRoom;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -16,4 +19,19 @@ public class AssignmentProblem extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "assignment_problem_start_date")
+    private LocalDateTime start_date;
+
+    @Column(name = "assigment_problem_due_date")
+    private LocalDateTime due_date;
+
+    @JoinColumn(name = "study_room_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private StudyRoom studyRoom;
+
+
+    @JoinColumn(name = "problem_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Problem problem;
 }
--- src/main/java/algogather/api/domain/assignment/AssignmentSolve.java
+++ src/main/java/algogather/api/domain/assignment/AssignmentSolve.java
@@ -1,10 +1,12 @@
 package algogather.api.domain.assignment;
 
 import algogather.api.domain.BaseTimeEntity;
+import algogather.api.domain.studyroom.StudyRoom;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import algogather.api.domain.user.User;
 
 import javax.persistence.*;
 
@@ -16,4 +18,12 @@ public class AssignmentSolve extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JoinColumn(name = "study_room_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private StudyRoom studyRoom;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 }
