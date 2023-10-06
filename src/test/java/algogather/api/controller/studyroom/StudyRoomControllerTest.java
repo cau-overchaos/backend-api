8761ec7e06b9488ede02342c1a4990004ef015b4
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-10-06 21:15:24 +0900

    test: 유저 제한수 넘겨서 스터디방 만들면 실패 테스트 작성

--- src/test/java/algogather/api/controller/studyroom/StudyRoomControllerTest.java
+++ src/test/java/algogather/api/controller/studyroom/StudyRoomControllerTest.java
@@ -104,4 +104,29 @@ class StudyRoomControllerTest {
         resultActions.andExpect(status().isUnauthorized())
                 .andExpect(jsonPath("$.message").value("접근 권한이 없습니다."));
     }
+
+    @Test
+    @DisplayName("유저수가 30명을 넘으가면 스터디방을 만들지 못한다.")
+    @WithUserDetails(value = "testUser", userDetailsServiceBeanName = "customUserDetailsService", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    void create_studyroom_exceed_max_user_fail() throws Exception {
+        //given
+        StudyRoomCreateForm studyRoomCreateForm = StudyRoomCreateForm
+                .builder()
+                .title("testStudyRoomTitle")
+                .description("testDescription")
+                .studyRoomVisibility(StudyRoomVisibility.PRIVATE)
+                .maxUserCnt(31)
+                .build();
+
+        //when
+        ResultActions resultActions = mvc.perform(post("/studyrooms")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(studyRoomCreateForm))
+                .accept(MediaType.APPLICATION_JSON)).andDo(print());
+
+        //then
+        resultActions.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("message").value("검증에 실패하였습니다. data 필드를 참고해주세요"))
+                .andExpect(jsonPath("$.data.maxUserCnt").value("유저의 수는 30명 이하이어야 합니다."));
+    }
 }
