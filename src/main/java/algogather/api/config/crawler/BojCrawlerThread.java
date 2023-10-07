d440890746eb4323894b8de4180207739f9b39f5
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-10-07 23:35:09 +0900

    test: BojCrawlerThread 간단하게 테스트

--- src/main/java/algogather/api/config/crawler/BojCrawlerThread.java
+++ src/main/java/algogather/api/config/crawler/BojCrawlerThread.java
@@ -1,5 +1,7 @@
 package algogather.api.config.crawler;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -9,8 +11,36 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-public class BojCrawler {
-    public List<Date> GetAcceptedDates(String userId, Long problemId) throws IOException {
+
+@Slf4j
+@Getter
+public class BojCrawlerThread implements Runnable{
+    private Thread thread;
+    private String userId;
+    private Long problemId;
+    private int ID;
+    List<Date> resultAcceptedDates;
+
+    public BojCrawlerThread(String userId, Long problemId, int ID) {
+        log.debug("BojCrawler #{} Created", ID);
+        this.userId = userId;
+        this.problemId = problemId;
+        this.ID = ID;
+
+        thread = new Thread(this::run);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            resultAcceptedDates = getAcceptedDates();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Date> getAcceptedDates() throws IOException {
         String urlFormat = "https://acmicpc.net/status?problem_id=%d&user_id=%s&language_id=-1&result_id=4&from_problem=1"; //result_id가 4여야 맞았습니다! 결과의 채점현황만 가져옴.
         String url = String.format(urlFormat, problemId, userId);
 
@@ -23,19 +53,16 @@ public class BojCrawler {
 
         Elements rows = doc.body().select("#status-table tbody tr");
         for (Element i: rows) {
-//            boolean ac = i.selectFirst(".result-ac") != null;
-//            if (ac) {
-                Element timeElement = i.selectFirst("[data-timestamp]");
-                if (timeElement == null)
-                    throw new NullPointerException();
+            Element timeElement = i.selectFirst("[data-timestamp]");
+            if (timeElement == null)
+                throw new NullPointerException();
 
-                long timestamp = Long.parseLong(timeElement.attr("data-timestamp")) * 1000L;
-                Date date = new Date(timestamp);
+            long timestamp = Long.parseLong(timeElement.attr("data-timestamp")) * 1000L;
+            Date date = new Date(timestamp);
 
-                //TODO 마감 날짜보다 오래된 날짜 나오면 중지
+            //TODO 마감 날짜보다 오래된 날짜 나오면 중지
 
-                result.add(date);
-//            }
+            result.add(date);
         }
 
         Element nextPageLink = doc.selectFirst("a#next_page");
@@ -46,4 +73,5 @@ public class BojCrawler {
 
         return result;
     }
+
 }
--- src/test/java/algogather/api/service/studyroom/AssignmentServiceTest.java
+++ src/test/java/algogather/api/service/studyroom/AssignmentServiceTest.java
@@ -1,5 +1,6 @@
 package algogather.api.service.studyroom;
 
+import algogather.api.config.crawler.BojCrawlerThread;
 import algogather.api.domain.assignment.AssignmentProblem;
 import algogather.api.domain.assignment.AssignmentProblemRepository;
 import algogather.api.domain.problem.Problem;
@@ -16,6 +17,10 @@ import javax.transaction.Transactional;
 import algogather.api.domain.user.User;
 import org.springframework.util.StopWatch;
 
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -97,6 +102,62 @@ class AssignmentServiceTest {
         for(int i = 0; i <100; i++) {
             assignmentService.checkIfUserSolveProblem(savedUser, savedAssignmentProblem);
 //            log.info("problme: {}, solved: {}", savedAssignmentProblem.getProblem().getPid(), assignmentService.checkIfUserSolveProblem(savedUser, savedAssignmentProblem));
+        }
+
+        stopWatch.stop();
+        log.info("time: {}", stopWatch.getTotalTimeSeconds());
+    }
+
+    @Test
+    void checkIfUserSolveProblem100timesMultiThreading() { // 쓰레드 100개로 100회 호출 약 2초
+        User testUser = User.builder()
+                .userId("testUserId")
+                .name("testUserName")
+                .password("testPassword")
+                .judge_account("boulce")
+                .role(UserRole.USER)
+                .build();
+        User savedUser = userRepository.save(testUser);
+
+        Problem problem = Problem.builder()
+                .pid(1000L)
+                .provider(ProblemProvider.BAEKJOON)
+                .build();
+
+        Problem savedProblem = problemRepository.save(problem);
+
+        AssignmentProblem assignmentProblem = AssignmentProblem.builder()
+                .problem(savedProblem)
+                .build();
+
+        AssignmentProblem savedAssignmentProblem = assignmentProblemRepository.save(assignmentProblem);
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        List<BojCrawlerThread> bojCrawlerThreadArrayList = new ArrayList<>();
+        for(int i = 0; i <50; i++) {
+            BojCrawlerThread bojCrawlerThread = new BojCrawlerThread(savedUser.getJudgeAccount(), assignmentProblem.getProblem().getPid(), i);
+            bojCrawlerThreadArrayList.add(bojCrawlerThread);
+        }
+
+        for(int i = 0; i <50; i++) {
+            BojCrawlerThread bojCrawlerThread = new BojCrawlerThread("smmaker118", assignmentProblem.getProblem().getPid(), i);
+            bojCrawlerThreadArrayList.add(bojCrawlerThread);
+        }
+
+        for(BojCrawlerThread b : bojCrawlerThreadArrayList) {
+            try {
+                b.getThread().join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        for(BojCrawlerThread b : bojCrawlerThreadArrayList) {
+            for (Date resultAcceptedDate : b.getResultAcceptedDates()) {
+                log.info("{}", resultAcceptedDate);
+            }
         }
 
         stopWatch.stop();
