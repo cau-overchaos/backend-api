0705d7395f5d8da1b6514ec43e56809d27cd87a2
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-11-03 10:45:34 +0900

    docs: 컨트롤러 응답 API 명세 data 내용 보이도록 개선, 요청 API 명세 UserAdapter 제외

--- src/main/java/algogather/api/controller/user/UserController.java
+++ src/main/java/algogather/api/controller/user/UserController.java
@@ -4,6 +4,9 @@ import algogather.api.domain.user.UserAdapter;
 import algogather.api.dto.api.ApiResponse;
 import algogather.api.dto.user.ProfileResponseDto;
 import algogather.api.service.user.UserService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +20,15 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "실패", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "접근 실패", content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+
+    })
     @GetMapping("/me")
-    public ResponseEntity<ApiResponse<?>> getMyProfileInfo(@AuthenticationPrincipal UserAdapter userAdapter) {
+    public ResponseEntity<ApiResponse<ProfileResponseDto>> getMyProfileInfo(@AuthenticationPrincipal UserAdapter userAdapter) {
         ProfileResponseDto profileInfo = userService.getProfileInfo(userAdapter);
 
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.sucessWithDataAndMessage(profileInfo, "프로필 정보를 성공적으로 불러왔습니다."));
