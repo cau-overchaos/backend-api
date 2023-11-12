559b59fd85f7024b57e6f16c680ef57db449b973
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-11-12 17:09:27 +0900

    feat: 스터디방 구인 게시글 RecruitPost, 스터디방 구인 댓글 RecruitComment 도메인 클래스 작성

---
+++ src/main/java/algogather/api/domain/recruit/RecruitComment.java
@@ -0,0 +1,41 @@
+package algogather.api.domain.recruit;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import algogather.api.domain.user.User;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class RecruitComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "comment_text")
+    private String text;
+
+    @JoinColumn(name = "recruit_post_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private RecruitPost recruitPost;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Builder
+    public RecruitComment(Long id, String text, RecruitPost recruitPost, User user) {
+        this.id = id;
+        this.text = text;
+        this.recruitPost = recruitPost;
+        this.user = user;
+    }
+}
+
+
---
+++ src/main/java/algogather/api/domain/recruit/RecruitPost.java
@@ -0,0 +1,45 @@
+package algogather.api.domain.recruit;
+
+import algogather.api.domain.studyroom.StudyRoom;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import algogather.api.domain.user.User;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class RecruitPost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "recruit_post_title")
+    private String title;
+
+    @Column(name = "recruit_post_text")
+    private String text;
+
+    @JoinColumn(name = "study_room_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private StudyRoom studyRoom;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Builder
+
+    public RecruitPost(Long id, String title, String text, StudyRoom studyRoom, User user) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.studyRoom = studyRoom;
+        this.user = user;
+    }
+}
