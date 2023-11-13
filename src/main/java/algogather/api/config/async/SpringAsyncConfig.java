cca4a0b511cf79838ee30bb6661ff783e36dc811
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-11-14 00:29:32 +0900

    크롤링 쓰레드 개수 1개로

--- src/main/java/algogather/api/config/async/SpringAsyncConfig.java
+++ src/main/java/algogather/api/config/async/SpringAsyncConfig.java
@@ -14,7 +14,7 @@ public class SpringAsyncConfig {
     @Bean(name = "threadPoolTaskExecutor")
     public Executor threadPoolTaskExecutor() {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(10); // 기본 스레드 수
+        taskExecutor.setCorePoolSize(1); // 기본 스레드 수
         taskExecutor.setMaxPoolSize(30); // 최대 스레드 수
         taskExecutor.setQueueCapacity(100); // Queue 사이즈;
         taskExecutor.setThreadNamePrefix("Executor-");
