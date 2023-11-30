3d02fa9a503914af3a147af3503fd97244bace81
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-11-30 16:20:24 +0900

    feat: PointRequestDto에 isDeleteAll 필드 추가

--- src/main/java/algogather/api/dto/draw/PointRequestDto.java
+++ src/main/java/algogather/api/dto/draw/PointRequestDto.java
@@ -9,6 +9,8 @@ import javax.validation.constraints.NotNull;
 @Setter
 public class PointRequestDto {
     @NotNull
+    private boolean isDeleteAll;
+    @NotNull
     private boolean isDelete;
     @NotNull
     private Long startX;
