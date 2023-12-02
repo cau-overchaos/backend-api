407461e3bf2f65db38c0f2181cc334ca5b6cd80f
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-12-02 17:30:44 +0900

    feat: 알림 목록 조회 API 구현

--- src/main/java/algogather/api/controller/notification/NotificationController.java
+++ src/main/java/algogather/api/controller/notification/NotificationController.java
@@ -2,26 +2,41 @@ package algogather.api.controller.notification;
 
 import algogather.api.domain.user.UserAdapter;
 import algogather.api.dto.api.ApiResponse;
+import algogather.api.dto.notification.NotificationListResponseDto;
 import algogather.api.dto.programminglanguage.ProgrammingLanguageListResponseDto;
 import algogather.api.service.notification.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/notifications/")
-@Tag(name = "프로그래밍 언어", description = "프로그래밍 언어 관련 API 입니다.")
+@RequestMapping("/notifications")
+@Tag(name = "알림", description = "알림 관련 API 입니다.")
 public class NotificationController {
-    private NotificationService notificationService;
+    private final NotificationService notificationService;
 
-//    public ResponseEntity<ApiResponse<>> findAll(@Parameter(hidden = true) @AuthenticationPrincipal UserAdapter userAdapter) {
-//
-//        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.sucessWithDataAndMessage(, "모든 프로그래밍 언어 목록을 조회했습니다."));
-//    }
+    @Operation(summary = "알림 목록 조회 API", description = "알림 목록 조회 API입니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "실패", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "인증 실패", content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+    })
+    @GetMapping
+    public ResponseEntity<ApiResponse<NotificationListResponseDto>> findAll(@Parameter(hidden = true) @AuthenticationPrincipal UserAdapter userAdapter) {
+
+        NotificationListResponseDto notificationListResponseDto = notificationService.getNotificationListResponseDto(userAdapter);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.sucessWithDataAndMessage(notificationListResponseDto, "현재 유저의 알림 목록을 정상적으로 조회했습니다."));
+    }
 }
