8de7590b66c604a0f24a6ab5c1d5c8168c9052ca
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-12-02 16:52:42 +0900

    feat: Notification 도메인 클래스 추가

---
+++ src/main/java/algogather/api/domain/notification/Notification.java
@@ -0,0 +1,32 @@
+package algogather.api.domain.notification;
+
+import algogather.api.domain.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import algogather.api.domain.user.User;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Notification extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "notification_content")
+    private String content;
+
+    @Column(name = "notification_is_new")
+    private Boolean isNew;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user
+            ;
+}
