08a6b6eb888d71c7d452f367fa36fed88d9a99c5
Habin Song <boulce.email@gmail.com>
2023-12-07 15:02:11 +0900

    fix: CORS method allow

--- src/main/java/algogather/api/config/cors/CorsFilter.java
+++ src/main/java/algogather/api/config/cors/CorsFilter.java
@@ -33,7 +33,7 @@ public class CorsFilter implements Filter {
         
         response.setHeader("Access-Control-Allow-Origin", CLIENT_URL);
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
